c1ea44f16180729392a6cd68c448a06421146fcb
Michael Gao <michaelzgao@gmail.com>
2023-04-14 20:56:56 -0700

    add grpc reflection

--- rust/api-tonic/src/server.rs
+++ rust/api-tonic/src/server.rs
@@ -1,10 +1,13 @@
 use tonic::{transport::Server, Request, Response, Status};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
+use proto::greeter_server::{Greeter, GreeterServer};
+use proto::{HelloReply, HelloRequest};
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld"); // The string specified here must match the proto package name
+mod proto {
+    tonic::include_proto!("helloworld");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("helloworld_descriptor");
 }
 
 #[derive(Debug, Default)]
@@ -18,7 +21,7 @@ impl Greeter for MyGreeter {
     ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
         println!("Got a request: {:?}", request);
 
-        let reply = hello_world::HelloReply {
+        let reply = proto::HelloReply {
             // We must use .into_inner() as the fields of gRPC requests and responses are private
             message: format!("Hello {}!", request.into_inner().name).into(),
         };
@@ -32,9 +35,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:50051".parse().unwrap();
     let greeter = MyGreeter::default();
 
+    let service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+
     println!("Server is online at {}", addr);
 
     Server::builder()
+        .add_service(service)
         .add_service(GreeterServer::new(greeter))
         .serve(addr)
         .await?;
