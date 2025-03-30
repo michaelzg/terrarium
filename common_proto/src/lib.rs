c1b7a5348be0152a330dba77ff5d95aeb93bf1b0
Michael Gao <michaelzgao@gmail.com>
2025-03-29 23:31:11 -0700

    cargo fmt

--- common_proto/src/lib.rs
+++ common_proto/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod proto {
     tonic::include_proto!("hello");
 
-    pub const FILE_DESCRIPTOR_SET: &[u8] =
-        tonic::include_file_descriptor_set!("hello_descriptor");
+    pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("hello_descriptor");
 }
